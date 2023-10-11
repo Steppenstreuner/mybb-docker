5e72a58246d1336782789c5564563a4eafc33db2
Steppenstreuner <hofmann47@gmx.net>
2023-10-11 10:44:45 +0200

    fixing path in bash.sh

--- docker_copy.sh
+++ docker_copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker cp api.php mybb-docker-mybb-1:/var/www/html/
-docker cp api/ mybb-docker-mybb-1:/var/www/html/
-docker cp inc/api.php mybb-docker-mybb-1:/var/www/html/inc
-docker cp inc/plugins/api.php mybb-docker-mybb-1:/var/www/html/inc/plugins
-docker cp inc/plugins/api/ mybb-docker-mybb-1:/var/www/html/inc/plugins
+docker cp /mybb/api.php mybb-docker-mybb-1:/var/www/html/
+docker cp /mybb/api/ mybb-docker-mybb-1:/var/www/html/
+docker cp /mybb/inc/api.php mybb-docker-mybb-1:/var/www/html/inc
+docker cp /mybb/inc/plugins/api.php mybb-docker-mybb-1:/var/www/html/inc/plugins
+docker cp /mybb/inc/plugins/api/ mybb-docker-mybb-1:/var/www/html/inc/plugins
